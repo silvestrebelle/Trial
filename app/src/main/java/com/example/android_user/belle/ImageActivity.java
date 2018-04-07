cc0c909e16ce4493087758edc8b592c3c0902ee4
android-user <android-user@fv.amacclab.edu.ph>
2018-04-07 17:19:16 +0800

    exercise #1

--- app/src/main/java/com/example/android_user/belle/ImageActivity.java
+++ app/src/main/java/com/example/android_user/belle/ImageActivity.java
@@ -1,13 +1,52 @@
 package com.example.android_user.belle;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Display;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
 
 public class ImageActivity extends AppCompatActivity {
+
+    private Button Button1, Button2, Button3;
+    private ImageView imagelah;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image);
+        initView();
+    }
+
+    private void initView(){
+        imagelah = findViewById(R.id.imagelah);
+        Button1 = findViewById(R.id.Button1);
+        Button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imagelah.setImageDrawable(getDrawable(R.drawable.maxresdefault));
+            }
+        });
+
+        Button2 = findViewById(R.id.Button2);
+        Button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imagelah.setImageDrawable(getDrawable(R.drawable.tumblr));
+            }
+        });
+
+        Button3 = findViewById(R.id.Button3);
+        Button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imagelah.setImageDrawable(getDrawable(R.drawable.y));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/android_user/belle/MainActivity.java
+++ app/src/main/java/com/example/android_user/belle/MainActivity.java
@@ -5,12 +5,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText Number_one, Number_two;
     private Button etButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
